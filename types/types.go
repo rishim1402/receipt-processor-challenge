d4d7aab724086125a90844b226978f8db0bff48d
Rishi Mundada <rishim1402@gmail.com>
2025-02-24 10:09:35 -0600

    feat: add validation tags to Receipt and Item types for improved data integrity

--- types/types.go
+++ types/types.go
@@ -1,14 +1,14 @@
 package types
 
 type Item struct {
-	ShortDescription string `json:"shortDescription"`
-	Price            string `json:"price"`
+	ShortDescription string `json:"shortDescription" binding:"required" validate:"required"`
+	Price            string `json:"price" binding:"required,numeric" validate:"required,numeric"`
 }
 
 type Receipt struct {
-	Retailer     string `json:"retailer"`
-	PurchaseDate string `json:"purchaseDate"`
-	PurchaseTime string `json:"purchaseTime"`
-	Items        []Item `json:"items"`
-	Total        string `json:"total"`
+	Retailer     string `json:"retailer" binding:"required" validate:"required"`
+	PurchaseDate string `json:"purchaseDate" binding:"required,datetime=2006-01-02" validate:"required,datetime=2006-01-02"`
+	PurchaseTime string `json:"purchaseTime" binding:"required,datetime=15:04" validate:"required,datetime=15:04"`
+	Items        []Item `json:"items" binding:"required,dive,required" validate:"required,min=1,dive,required"`
+	Total        string `json:"total" binding:"required,numeric" validate:"required,numeric"`
 }
